5805ce2392ac88d320d7ec3b768832eb56fdef9f
David Petersen <thedjpetersen@gmail.com>
2011-03-20 16:02:15 -0700

    Count number of credits taken

--- parse_html.py
+++ parse_html.py
@@ -17,6 +17,14 @@ def get_grades(original_html):
         for subelement in element.getchildren():
             text.append(subelement.text_content())
         
-        classes[index] = {'Name' : text[2], 'Department' : text[0], 'Course Number' : text[1], 'Credits' : text[3], 'Grade' : text[4]}
+        classes[index] = {'Name' : text[2], 'Department' : text[0], 'Course Number' : text[1], 'Credits' : float(text[3]), 'Grade' : text[4]}
 
     return classes
+
+def get_total_credits(original_html):
+    html = lxml.html.fromstring(original_html)
+    credits = 0
+    for element in html.find_class("ddlabel"):
+        if element.text_content() == 'Overall:':
+            credits = float(element.getnext().getchildren()[0].text_content())
+    return credits
--- transcript.py
+++ transcript.py
@@ -3,3 +3,4 @@ import parse_html
 class Transcript:
     def __init__(self, html):
         self.grades = parse_html.get_grades(html)
+        self.credits = parse_html.get_total_credits(html)
