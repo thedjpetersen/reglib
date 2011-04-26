3a9cf75f671b125034740452de4214fd270dd64b
David Petersen <david@bruiser.(none)>
2011-04-25 18:26:00 -0700

    Adding documentation

--- registration.py
+++ registration.py
@@ -105,24 +105,32 @@ class infosu(object):
         if response.url == 'http://catalog.oregonstate.edu/DOE.aspx?Entity=Course':
             return "Class not found"
         html = response.read()
+
+        # Get a array of available classes
         classes = parse_html.class_search(html)
         if term is '':
             return classes
+        
+        # Get classes from certain term
         else:
             list_of_classes = []
             for each_class in classes:
                 if each_class['Term'] == term:
+                    #If the classes are in a certain term return them
                     list_of_classes.append(each_class)
                 if len(list_of_classes) is not 0:
                     return list_of_classes
                 else:
                     return "No classes offered for that term"
 
+    # This function searches for classes that don't conflict with your 
+    # current schedule
     def class_search_schedule(self, dep, num):
         terms = {'01':'F', '02':'W', '03':'Sp', '04':'Su'}
         term = terms[self.current_term[-2:]] + self.current_term[2:4]
     
         search_results = self.class_search(dep, num, term)
+        # If no classes are found, the function will return a string
         if type(search_results) is str:
             return search_results
         
@@ -130,6 +138,7 @@ class infosu(object):
 
         for result in search_results:
             flag = False
+            # If the class is full
             if result['Avail'] <= '0':
                 print "Full class"
                 continue
@@ -139,31 +148,38 @@ class infosu(object):
                 curr_days = current_class['Days']
                 curr_time = current_class['Time']
                 if(set(result_days).intersection(curr_days)):
+                    #If we have a conflict set the flag 
                     if self.conflict(result_times, curr_time): 
                         print ('-').join(result_times) + " conflicts with " + ('-').join(curr_time)
                         flag = True
                     else:
                         print ('-').join(result_times) + " does not conflict with " + ('-').join(curr_time)
             if not flag:
+                # If no conflict appned to results(flag unset)
                 available_classes.append(result)
 
         return available_classes
 
+    # Helper function that will determine whether or not two times conflict
+    # Format of time is ['13:00', '13:50']
     def conflict(self, time, time1):
         if (time[0] >= time1[0] and time[0] <= time1[1]) or (time[1] >= time1[0] and time[1] <= time1[1]):
             return True
         return False
 
+    # Function that retrieves the mydegrees page
     def get_major_requirements(self):
         form_page = "https://adminfo.ucsadm.oregonstate.edu/prod/bwykg_dwssbstudent.P_SignOn"
         self.header_values['Referer'] = "https://adminfo.ucsadm.oregonstate.edu/prod/twbkwbis.P_GenMenu?name=bmenu.P_AdminMnu"
 
+        # Get request page for the form
         request = urllib2.Request(form_page, headers = self.header_values)
         response = self.opener.open(request)
         html = response.read()
         form_list = parse_html.mydegrees_redirect_form(html)
         form_data = urllib.urlencode(form_list)
 
+        # Get first page from mydegrees
         mydegrees_url = "https://mydegrees.oregonstate.edu/IRISLink.cgi"
         self.header_values['Referer'] = "https://adminfo.ucsadm.oregonstate.edu/prod/bwykg_dwssbstudent.P_SignOn"
         
@@ -173,28 +189,32 @@ class infosu(object):
         self.header_values['Referer'] = "https://mydegrees.oregonstate.edu/SD_LoadFrameForm.html"
 
         form_data = urllib.urlencode({'SERVICE':'SCRIPTER','SCRIPT':'SD2STUCON'})
-        #phase one give your data bitch
         request = urllib2.Request(mydegrees_url, form_data, headers = self.header_values)
         response = self.opener.open(request)
         html = response.read() #final form is in html.forms[7].form_values()
 
+        # Get variables from mydegrees
         form_list = parse_html.mydegrees_form_mangler(html)
         form_data = urllib.urlencode(form_list)
         request = urllib2.Request(mydegrees_url, form_data, headers= self.header_values)
         response = self.opener.open(request)
         html = response.read()
 
+        # Get more variables from mydegrees
         form_list = parse_html.mydegrees_final_form(html)
+        # Construct variables to post 
         form_data = urllib.urlencode(form_list)
         request = urllib2.Request(mydegrees_url, form_data, headers= self.header_values) 
-
+        # Make request for degree audit xml from mydegrees
         response = self.opener.open(request)
         xml = response.read()
         
         return parse_html.get_major_requirements(xml)
 
+    # Function to add class to a schedule
     def add_class(self, crn, crn2=''):
         for i in range(self.login_number):
+            #Page with the form to add/drop classes
             add_drop_page_url = 'https://adminfo.ucsadm.oregonstate.edu/prod/bwskfreg.P_AltPin'
             self.header_values['Referer'] = 'https://adminfo.ucsadm.oregonstate.edu/prod/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu'
             request = urllib2.Request(add_drop_page_url, headers = self.header_values)
@@ -215,16 +235,20 @@ class infosu(object):
             response = self.opener.open(request)
             html = response.read()
             
+            # Get a dictionary of values to post as the form
             values = parse_html.add_class(html, crn, crn2)
            
+            #Set up data to be posted
             form_data = urllib.urlencode(values)
             self.header_values['Referer'] = 'https://adminfo.ucsadm.oregonstate.edu/prod/bwskfreg.P_AltPin'
             submit_url = 'https://adminfo.ucsadm.oregonstate.edu/prod/bwckcoms.P_Regs'
 
             request = urllib2.Request(submit_url, form_data, headers=self.header_values)
+            # Request page with CRNs of classes to add
             response = self.opener.open(request)
 
             html = response.read()
             
+            # See if there were any errors when posting the form
             return parse_html.add_class_has_errors(html) 
 
