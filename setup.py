c69bc46a9d08406e57de326d6abbb32d7e7e1107
David Petersen <thedjpetersen@gmail.com>
2011-05-01 17:49:33 -0700

    Added setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='registration',
+        version='0.1',
+        description='Library to interface with Oregon State\'s registration system',
+        author='David Petersen',
+        author_email='thedjpetersen@gmail.com',
+        )
