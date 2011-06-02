a92ddd2d1b1c872315bdb2639ef9df6ca3e39741
Kevin Ngo <ktngo09@gmail.com>
2011-06-01 21:17:05 -0700

    changed dict keys back for mydegrees

--- test.py
+++ test.py
@@ -4,7 +4,10 @@ import reglib
 # INSTANTIATION
 ###############
 reg_class = reglib.infosu('931596171', '544354')
-#results = reg_class.make_schedule(['cs 321'], 'F11')
+#print reg_class
+#result = reg_class.make_schedule(['ph 212'],'F11')
+#for course in result:
+#    print course
 
 #for index, combination_list in enumerate(results):
 #    print "Combination " + str(index+1)
@@ -31,7 +34,7 @@ reg_class.get_transcript()
 ##########
 #SEARCHING
 ##########
-print reg_class.class_search('cd', '311')
+#print reg_class.class_search('cd', '311')
 #print reg_class.class_search_schedule('ph', '213')
 
 
@@ -41,4 +44,12 @@ print reg_class.class_search('cd', '311')
 #print reg_class.make_schedule(['cs 261', 'fr 213', 'cs 275', 'mth 232'])
 #print reg_class.make_schedule(['cs 162', 'mth 111'], 'F12')
 
+##########
+#MYDEGREES
+##########
+audit = reg_class.get_major_requirements()
+#print audit
+for instance in audit.required_classes:
+    for course in instance.courses:
+        print course
 
