31374fe5ce08f486f2a112ec36a031f3b0e30c45
Kevin Ngo <ktngo09@gmail.com>
2011-06-03 02:00:54 -0700

    added exception handling to description regex

--- reglib/utilities/parse_html/class_search.py
+++ reglib/utilities/parse_html/class_search.py
@@ -10,7 +10,13 @@ def class_search(original_html, dep, num):
     #description_regex_bacc = re.compile('\(\d\)\.\s*?<img .*?>\s*?</h3>\s+([\w\s/,\.]+)')
     description_regex = re.compile('\(\d\)\.\s*?(<img .*?>)?\s*?</h3>\s+([\w\s/,\.]+)') # handles bacc core classes
     title = title_regex.findall(original_html)[0]
-    description = description_regex.findall(original_html)[0][1]
+
+
+    description = description_regex.findall(original_html)
+    if description:
+        description = description[0]
+    if description:
+        description = description[1]
 
     try:
         table_element  = html.get_element_by_id('ctl00_ContentPlaceHolder1_SOCListUC1_gvOfferings')
