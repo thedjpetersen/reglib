c4cffeea95f4baeaf5ade56a6301b89346ec280e
David Petersen <thedjpetersen@gmail.com>
2011-06-07 08:36:05 -0700

    Fixed reference error

--- reglib/utilities/class_search_conflict.py
+++ reglib/utilities/class_search_conflict.py
@@ -5,6 +5,6 @@ def class_search_conflict(class1, class2):
 
     # some exception handling
     if 'days' in class1 and 'days' in class2 and 'times' in class1 and 'times' in class2:
-        if (set(class1['days']).intersection(class2['days']) and utilities.time_conflict(class1['times'], class2['times'])):
+        if (set(class1['days']).intersection(class2['days']) and time_conflict(class1['times'], class2['times'])):
             return True
     return False
